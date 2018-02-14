acbf863054c83da5c8f53bdfe15a6386c0bd7dd2
Antoine Gagné <gagnantoine@gmail.com>
2018-02-13 19:18:56 -0500

    Add some Java VM options

--- bin/ltsa.sh
+++ bin/ltsa.sh
@@ -1,4 +1,21 @@
 #! /usr/bin/env sh
 
-java -splash:/opt/ltsatool/assets/splash.png \
-     -jar /opt/ltsatool/lib/ltsa.jar
+LOG_DIRECTORY="${HOME}/.log/ltsa"
+ERRORS_LOG_FILE="${LOG_DIRECTORY}/errors.log"
+LOG_FILE="${LOG_DIRECTORY}/ltsa.log"
+HEAP_DUMP_FILE="${LOG_DIRECTORY}/ltsa-errors.hprof"
+
+
+main() {
+    mkdir -p "${LOG_DIRECTORY}"
+
+    java -splash:/opt/ltsatool/assets/splash.png \
+         -Xmx512m \
+         -XX:+UnlockDiagnosticVMOptions \
+         -XX:ErrorFile="${ERRORS_LOG_FILE}" \
+         -XX:LogFile="${LOG_FILE}" \
+         -XX:HeapDumpPath="${HEAP_DUMP_FILE}" \
+         -jar /opt/ltsatool/lib/ltsa.jar
+}
+
+main
